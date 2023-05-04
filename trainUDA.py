b93a983749eca6d6eb2e6960f0a44c3863ac9e42
DuongKien2001 <trungkienty2001@gmail.com>
2023-05-05 00:36:59 +0700

    k

--- trainUDA.py
+++ trainUDA.py
@@ -10,6 +10,8 @@ import pickle
 import scipy.misc
 from core.configs import cfg
 
+import cv2
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -569,7 +571,7 @@ def main():
         src_mask = src_mask.contiguous().view(B * Hs * Ws, )
         assert not src_mask.requires_grad
         _, _, Ht, Wt = tgt_feat.size()
-        tgt_out_maxvalue, tgt_mask = torch.max(tgt_out_ema, dim=1)
+        tgt_mask = cv2.resize(targets_u, (65,65), interpolation = cv.INTER_NEAREST)
         print(tgt_mask.size())
         tgt_mask = tgt_mask.contiguous().view(B * Ht * Wt, )
         assert not tgt_mask.requires_grad
@@ -583,7 +585,9 @@ def main():
         feat_estimator.update(features=src_feat_ema.detach(), labels=src_mask)
 
         # contrastive loss on both domains
+        pixelWiseWeight = cv2.resize(pixelWiseWeight, (65,65), interpolation = cv.INTER_NEAREST)
         print(pixelWiseWeight)
+
         loss_feat = pcl_criterion_src(Proto=feat_estimator.Proto.detach(),
                                   feat=src_feat,
                                   labels=src_mask) \
